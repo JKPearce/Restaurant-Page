010b43b17ddc9aeb5796c33cb28b8e470575eaed
Jaydon <pearcejaydon@gmail.com>
2022-08-14 22:55:40 +1000

    Create nav bar and add styles to nav bar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import "./style.scss";
 
-console.log("test");
+const contentElement = document.getElementById('content');
+const navElement = document.createElement('nav');
+const homeDiv = document.createElement('div');
+const menuDiv = document.createElement('div');
+const contactDiv = document.createElement('div');
+
+homeDiv.textContent = "Home";
+menuDiv.textContent = "Menu";
+contactDiv.textContent = "Contact";
+
+
+navElement.appendChild(homeDiv);
+navElement.appendChild(menuDiv);
+navElement.appendChild(contactDiv);
+
+contentElement.appendChild(navElement);
