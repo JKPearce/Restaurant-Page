008e63b2f8be78fc39fab80dc9ec6541b13f5d3e
Jaydon <pearcejaydon@gmail.com>
2022-08-14 23:21:01 +1000

    Add content and styles to content

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./style.scss";
 
+//create navbar
 const contentElement = document.getElementById('content');
 const navElement = document.createElement('nav');
 const homeDiv = document.createElement('div');
@@ -16,3 +17,18 @@ navElement.appendChild(menuDiv);
 navElement.appendChild(contactDiv);
 
 contentElement.appendChild(navElement);
+
+//create Home Content
+const headlineElement = document.createElement('div');
+const aboutElement = document.createElement('div');
+
+headlineElement.textContent= 'Awesome restaurant';
+headlineElement.classList.add('title');
+aboutElement.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident suscipit commodi ex soluta accusamus ad ipsum, cumque deserunt exercitationem consectetur voluptas asperiores qui ea aspernatur animi molestiae iusto pariatur itaque sint voluptatibus quasi. Quia, ab! Ex, odio! Sunt, aspernatur repellat.";
+aboutElement.classList.add('about')
+
+
+contentElement.appendChild(headlineElement);
+contentElement.appendChild(aboutElement);
+
+console.log("gh")
